d0743c851ed58dc8da10022b44f419f490e5b3be
Maocampog1 <maocampog1@eafit.edu.co>
2025-08-24 11:31:52 -0500

    feat(modelos+admin): definir modelos de catálogo y configuración de admin (búsqueda/filtros/migraciones)

--- producto/admin.py
+++ producto/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Producto, Categoria
 
-# Register your models here.
+# Autor: Maria Alejandra Ocampo
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "slug")
+    prepopulated_fields = {"slug": ("nombre",)}
+    search_fields = ("nombre",)
+
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "precio", "cantidad_disp", "es_activo")
+    list_filter = ("es_activo", "categorias")
+    search_fields = ("nombre", "descripcion")
+    filter_horizontal = ("categorias",)  # UI cómoda para ManyToMany
