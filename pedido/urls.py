378bfb71a8da36b30396cbe50e01a03aa0fc13fa
Luis <laneriop@eafit.edu.co>
2025-08-28 16:18:22 -0500

    feat: añadir vista básica de carrito con ruta

--- pedido/urls.py
+++ pedido/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.views.generic import RedirectView
+from .views import CarritoDetalleView
 
 app_name = 'pedido'
 
 urlpatterns = [
     # nombre requerido por {% url 'pedido:carrito' %} → redirige a home
-    path('carrito/', RedirectView.as_view(pattern_name='usuario:home'), name='carrito'),
+    path('carrito/', CarritoDetalleView.as_view(), name='carrito'),
+    path('carrito_detalle/', CarritoDetalleView.as_view(), name='carrito_detalle'),
 ]
--- pedido/views.py
+++ pedido/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 
-# Create your views here.
+class CarritoDetalleView(TemplateView):
+    """Vista básica del carrito: por ahora solo renderiza un template vacío."""
+    template_name = "carrito_detalle.html"
+
