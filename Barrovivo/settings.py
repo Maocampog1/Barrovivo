dde412fbe22d69bd2f827af0b461ad0603fda687
luisNP21 <157830657+luisNP21@users.noreply.github.com>
2025-11-04 00:58:54 -0500

    Add Whitenoise for static file storage

--- Barrovivo/settings.py
+++ Barrovivo/settings.py
@@ -63,6 +63,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 ROOT_URLCONF = 'Barrovivo.urls'
